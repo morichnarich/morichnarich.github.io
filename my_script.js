005a72fa6aa4c50592ce997f9ee2645ba4ae3666
Narichika Morinaga <morichnarich@gmail.com>
2020-11-23 19:35:53 +0900

    Site updated: 2020-11-23 19:35:46

---
+++ my_script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * Generate a sharing link
+ */
+// const js = hexo.extend.helper.get('js').bind(hexo);
+// hexo.extend.injector.register('body_end', () => {
+//   return js('../scripts/injectors/post.js');
+// });
+var postToggles = document.getElementsByClassName('postToggle')
+var postToggleds = document.getElementsByClassName('postToggled')
+    postToggles = [].slice.call(postToggles)
+
+postToggleds = [].slice.call(postToggleds)
+for(let i=0; i<postToggles.length; i++) {
+    postToggles[i].addEventListener('click', postTog, false)
+}
+function postTog(e) {
+    
+    const eTarget = e.currentTarget.nextElementSibling
+
+    if (eTarget.hasAttribute('style')) {
+        eTarget.removeAttribute('style')
+    } else {
+        eTarget.style.setProperty('position', 'absolute')
+        eTarget.style.setProperty('top', '-100000vh')
+    } 
+}
